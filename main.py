d2462e0cfd694d1fe4bcbf79e7809d38d9765973
Julio Daniel Azurduy Montalvo <jazurduy@gmail.com>
2023-10-24 15:46:02 +0200

    Nav and footer

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, Flask, redirect, url_for
-import os
+import psycopg2
 
 app = Flask(__name__)
 
+# Configurar la conexión a la base de datos PostgreSQL
+#conn = psycopg2.connect(
+   # database="mydb",
+  #  user="miusuario",
+ #   host="localhost"
+#)
+
 @app.route('/')
 def home():
+    # Realizar una consulta SQL a la base de datos
+    #cursor = conn.cursor()
+    #cursor.execute("SELECT * FROM tu_tabla")
+    #resultados = cursor.fetchall()
+    #cursor.close()
+#    return render_template('index.html', resultados=resultados)
     return render_template('index.html')
 
 if __name__ == '__main__':
