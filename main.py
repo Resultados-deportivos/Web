ebaf98167535146afd520136453fdd414987fdb5
Julio Daniel Azurduy Montalvo <jazurduy@gmail.com>
2023-10-25 09:23:26 +0200

    Estrucutura html

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, Flask, redirect, url_for
 import psycopg2
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
 
 # Configurar la conexión a la base de datos PostgreSQL
 #conn = psycopg2.connect(
@@ -10,7 +11,7 @@ app = Flask(__name__)
  #   host="localhost"
 #)
 
-@app.route('/')
+@app.route('/es/index.html')
 def home():
     # Realizar una consulta SQL a la base de datos
     #cursor = conn.cursor()
@@ -19,6 +20,12 @@ def home():
     #cursor.close()
 #    return render_template('index.html', resultados=resultados)
     return render_template('index.html')
+@app.route('/es/')
+def index_es():
+    return redirect(url_for('home'))
+@app.route('/')
+def index():
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
