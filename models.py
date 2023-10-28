f469465191a9195500bd64138c8815d8dbd0ce8c
Julio Daniel Azurduy Montalvo <jazurduy@gmail.com>
2023-10-28 11:15:41 +0200

    BD work with psycopg2

--- models.py
+++ models.py
@@ -1,61 +1,48 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import relationship
+import psycopg2
 
+# Database connection parameters
 db_connection = {
     "database": "eusko_basket",
     "user": "admin_basket",
     "password": "Reto@123",
     "host": "pgsql03.dinaserver.com",
-    "port": "5432"
+    "port": "5432",
 }
 
-engine = create_engine(
-    f'postgresql://{db_connection["user"]}:{db_connection["password"]}@{db_connection["host"]}:{db_connection["port"]}/{db_connection["database"]}'
+# Create a connection to the database
+conn = psycopg2.connect(
+    database=db_connection["database"],
+    user=db_connection["user"],
+    password=db_connection["password"],
+    host=db_connection["host"],
+    port=db_connection["port"],
 )
 
-Base = declarative_base()
+# Create a cursor
+cur = conn.cursor()
 
+# Define a class to represent Publicacion objects
+class Publicacion:
+    def __init__(self, id, img, titulo, descripcion):
+        self.id = id
+        self.img = img
+        self.titulo = titulo
+        self.descripcion = descripcion
 
-class Publicacion(Base):
-    __tablename__ = 'Publicaciones'
-    Id = Column(Integer, primary_key=True, autoincrement=True)
-    Img = Column(String(255))
-    Titulo = Column(String(255))
-    Descripcion = Column(String(255))
+# Execute a SQL query to fetch Publicacion records
+cur.execute("SELECT * FROM publicaciones")
 
+# Fetch all results and map them to Publicacion objects
+results = []
+for row in cur.fetchall():
+    id, img, titulo, descripcion = row
+    publicacion = Publicacion(id, img, titulo, descripcion)
+    results.append(publicacion)
 
-class Comentario(Base):
-    __tablename__ = 'Comentarios'
-    Id = Column(Integer, primary_key=True, autoincrement=True)
-    Idusuario = Column(Integer, ForeignKey('Usuarios.Id'))
-    Publicacionid = Column(Integer, ForeignKey('Publicaciones.Id'))
-    Descripcion = Column(Text)
+# Close the cursor and connection
+cur.close()
+conn.close()
 
-
-class Like(Base):
-    __tablename__ = 'Likes'
-    Id = Column(Integer, primary_key=True, autoincrement=True)
-    Publicacionid = Column(Integer, ForeignKey('Publicaciones.Id'))
-    Usuarioid = Column(Integer, ForeignKey('Usuarios.Id'))
-    LikeCount = Column(Integer)
-
-
-if __name__ == '__main__':
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    results = session.query(Publicacion).all()
-    for result in results:
-        print(
-            f"Id: {result.Id}, Img: {result.Img.decode('latin-1')}, Titulo: {result.Titulo.decode('latin-1')}, Descripcion: {result.Descripcion.decode('latin-1')}")
-
-    for result in results:
-        try:
-            img = result.Img.decode('utf-8')
-            titulo = result.Titulo.decode('utf-8')
-            descripcion = result.Descripcion.decode('utf-8')
-            print(f"Id: {result.Id}, Img: {img}, Titulo: {titulo}, Descripcion: {descripcion}")
-        except UnicodeDecodeError as e:
-            print(f"Error decoding data: {e}")
-
+# Print the results
+for publicacion in results:
+    print(f"ID: {publicacion.id}, Img: {publicacion.img}, Titulo: {publicacion.titulo}, Descripcion: {publicacion.descripcion}")
